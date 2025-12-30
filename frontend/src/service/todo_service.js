8253998986571af97fe75474fbc0c0bf2a705ae1
Kanishk Dad <137789901+kanishk-23@users.noreply.github.com>
2025-12-30 16:36:38 +0530

    Switch BASE_URL to use environment variable
    updating BASE_URL from local to environment

--- frontend/src/service/todo_service.js
+++ frontend/src/service/todo_service.js
@@ -1,6 +1,5 @@
 import axios from 'axios';
-// const BASE_URL = `${process.env.REACT_APP_BASE_URL}/api/todos`;
-const BASE_URL = `http://localhost:4000/api/todos`;
+const BASE_URL = `${process.env.REACT_APP_BASE_URL}/api/todos`;
 export const getTodos = () => axios.get(BASE_URL);
 export const createTodo = (data) => axios.post(BASE_URL, data);
 export const updateTodo = (id, data) => axios.put(`${BASE_URL}/${id}`, data);
